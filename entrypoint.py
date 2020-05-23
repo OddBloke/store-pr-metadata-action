47b725b22b53c396b0d3d9261fb538cd7dd7a3f4
Daniel Watkins <daniel@daniel-watkins.co.uk>
2020-05-22 21:05:53 -0400

    side-eye

--- entrypoint.py
+++ entrypoint.py
@@ -10,5 +10,5 @@ g = Github(sys.argv[1])
 
 repo_name = os.environ["GITHUB_REPOSITORY"]
 repo = g.get_repo(repo_name)
-pr = repo.get_pull(sys.argv[2])
+pr = repo.get_pull(int(sys.argv[2]))
 print(str(pr))
