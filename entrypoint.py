3e044acbaa98944207e2382274e80a004adabc9a
Daniel Watkins <daniel@daniel-watkins.co.uk>
2020-05-22 22:01:59 -0400

    start seeing about the git bit

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import subprocess
 import sys
 
 from github import Github
@@ -12,3 +13,17 @@ repo_name = os.environ["GITHUB_REPOSITORY"]
 repo = g.get_repo(repo_name)
 pr = repo.get_pull(int(sys.argv[2]))
 print(str(pr))
+
+remote_repo = (
+    "https://{GITHUB_ACTOR}:{token}@github.com/{GITHUB_REPOSITORY}.git".format(
+        token=sys.argv[1],
+        **os.environ
+    )
+)
+
+process = subprocess.run(["git", "clone", remote_repo], capture_output=True)
+print(process.stdout)
+print(process.returncode)
+print(process.stderr)
+
+print(os.environ)
